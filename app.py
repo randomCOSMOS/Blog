0e0b155dfd1f4c590cb1e80ece45c07ba4084e79
RANDOM cosmos <srijanwhat2@gmail.com>
2020-02-16 00:40:41 +0530

    Added edit page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -10,65 +10,89 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
-    def __init__(self, name, location):
-        self.name = name
-        self.location = location
+# table
+class Post(db.Model):
+    def __init__(self, heading, subtitle, article, author):
+        self.heading = heading
+        self.subtitle = subtitle
+        self.article = article
+        self.author = author
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    location = db.Column(db.String(50))
-    date_created = db.Column(db.DateTime, default=datetime.now)
+    heading = db.Column(db.String(50))
+    subtitle = db.Column(db.String(50))
+    article = db.Column(db.String(100))
+    author = db.Column(db.String(20))
+    datetime = db.Column(db.DateTime, default=datetime.utcnow())
 
 
+# home page
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def home():
+    posts = Post.query.all()
+    return render_template('home.html', posts=posts)
 
 
-@app.route('/add', methods=['post'])
-def add_to_db():
-    user = request.form['user']
-    location = request.form['location']
-
-    new_task = User(user, location)
-    try:
-        db.session.add(new_task)
-        db.session.commit()
-    except:
-        return "Error Occurred"
-
-    return redirect('/')
-
-
-@app.route('/delete page')
-def show():
-    return render_template('delete.html')
-
-
-@app.route('/delete', methods=['post'])
-def delete():
-    user = request.form['user']
-    if user == 'all':
-        User.query.delete()
-        db.session.commit()
-
-        return redirect('/delete page')
+# posting page
+@app.route('/post', methods=['POST', 'GET'])
+def post():
+    if request.method == 'GET':
+        return render_template('post.html')
     else:
-        remove = User.query.filter_by(name=user).first()
+        data = request.form
+        heading = data['heading']
+        subtitle = data['subtitle']
+        article = data['article']
+        author = data['author']
 
-        if remove is None:
-            return render_template('delete.html', error="Cant find that")
-        else:
-            db.session.delete(remove)
-            db.session.commit()
-            return redirect('/delete page')
+        new_post = Post(heading, subtitle, article, author)
+        db.session.add(new_post)
+        db.session.commit()
+
+        return redirect('/post')
 
 
-@app.route('/data')
-def data():
-    users = User.query.all()
-    return render_template('see.html', users=users)
+# article page
+@app.route('/article/<id>')
+def ok(id):
+    this_post = Post.query.get(id)
+    return render_template('article.html', post=this_post)
+
+
+# manage page
+@app.route('/manage')
+def manage():
+    posts = Post.query.all()
+    return render_template('manage.html', posts=posts)
+
+
+# delete function
+@app.route('/delete/<id>')
+def delete(id):
+    delete_post = Post.query.get(id)
+    if delete_post is None:
+        error = "Can't find post with id {}".format(id)
+        return render_template('manage.html', error=error)
+    else:
+        db.session.delete(delete_post)
+        db.session.commit()
+        return redirect('/manage')
+
+
+@app.route('/edit/<id>', methods=['POST', 'GET'])
+def edit(id):
+    edit_post = Post.query.get(id)
+    if request.method == 'GET':
+        return render_template('edit.html', post=edit_post)
+    else:
+        data = request.form
+
+        edit_post.heading = data['heading']
+        edit_post.subtitle = data['subtitle']
+        edit_post.article = data['article']
+        edit_post.author = data['author']
+        db.session.commit()
+        return redirect('/manage')
 
 
 if __name__ == '__main__':
