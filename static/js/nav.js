908f6ff3ec0dec82b88dc0960cf32db4b403af57
RANDOM cosmos <srijanwhat2@gmail.com>
2020-04-02 00:57:03 +0530

    test

--- static/js/nav.js
+++ static/js/nav.js
@@ -2,3 +2,8 @@ $('.hamburger').click(() => {
     $('.hamburger').toggleClass('is-active');
     $('nav div').toggleClass('show');
 });
+
+// var loc = window.location.href + '';
+// if (loc.indexOf('http://') == 0) {
+//     window.location.href = loc.replace('http://', 'https://');
+// }
